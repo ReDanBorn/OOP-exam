fda5e0619310183c291f353ff155666c7a5dd12a
ReDanBorn <154323169+ReDanBorn@users.noreply.github.com>
2024-06-27 08:10:25 +0300

    +35 номер

---
+++ OOP-exam/OOP-exam/35.cpp
@@ -0,0 +1,15 @@
+#include <iosteam>
+using namespace std;
+
+template<typename T1, typename T2> // Шаблон функции
+T2 Sum(T1 a, T2 b)
+{
+  return a+b;
+} 
+
+
+int main() 
+{
+  setlocale(LC_ALL, "RU");
+  cout << Sum(5, 14.5) << endl;
+} 
